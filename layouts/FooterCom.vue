fa4e4afebad3c52faf194330903e4c06d87756cc
git-won <142965904+git-won@users.noreply.github.com>
2025-01-24 11:02:21 +0900

    src/layouts

---
+++ layouts/FooterCom.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="footer">
+    <div class="footer_top">
+      <FooterLogo></FooterLogo>
+      <button @click="toggleAccordion"><span>▼</span></button>
+    </div>
+    <div v-if="isAccordionOpen" class="accordion_content">
+      <ul>
+        <li v-for="item in companyInfo" :key="item">{{ item }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import FooterLogo from "@/components/footer/FooterLogo.vue";
+
+export default defineComponent({
+  name: "FooterCom",
+  components: {
+    FooterLogo,
+  },
+  data() {
+    return {
+      companyInfo: {
+        대표이사: "홍길동",
+        설립연도: 1999,
+        위치: "서울특별시",
+        직원수: 100,
+        업종: "IT",
+      },
+    };
+  },
+  setup() {
+    const isAccordionOpen = ref(false);
+
+    const toggleAccordion = () => {
+      isAccordionOpen.value = !isAccordionOpen.value;
+    };
+
+    return {
+      isAccordionOpen,
+      toggleAccordion,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+#footer {
+  background-color: #111;
+  padding: 20px $margin 40px;
+  max-width: 750px;
+  .footer_top {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 6px 0;
+    button {
+      color: #fff;
+      background-color: transparent;
+      border: none;
+      span {
+        display: block;
+        width: 24px;
+        height: 24px;
+        font-size: 16px;
+      }
+    }
+  }
+  .accordion_content {
+    background-color: #222;
+    color: #fff;
+    padding: 10px;
+    transition: all 0.3s ease-in-out;
+  }
+}
+</style>
---
+++ layouts/HeaderCom.vue
@@ -0,0 +1,55 @@
+<template>
+  <header class="header">
+    <div class="header_top">
+      <HeaderLogo />
+      <HeaderIcons></HeaderIcons>
+    </div>
+    <HeaderSearch></HeaderSearch>
+    <HeaderMenu></HeaderMenu>
+    <!-- <HamburgerButton @click="toggleMenu" /> -->
+    <!-- <SlideMenu :isOpen="isMenuOpen" @close="toggleMenu" /> -->
+  </header>
+</template>
+
+<script>
+import HeaderLogo from "@/components/header/HeaderLogo.vue";
+/* import HamburgerButton from "@/components/header/HamburgerButton.vue";
+import SlideMenu from "@/components/header/SlideMenu.vue"; */
+import HeaderIcons from "@/components/header/HeaderIcons.vue";
+import HeaderMenu from "@/components/header/HeaderMenu.vue";
+import HeaderSearch from "@/components/header/HeaderSearch.vue";
+
+export default {
+  name: "HeaderCom",
+  components: {
+    HeaderLogo,
+    /* HamburgerButton, */
+    /* SlideMenu, */
+    HeaderIcons,
+    HeaderMenu,
+    HeaderSearch,
+  },
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  .header_top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: $margin;
+  }
+  background-color: #111;
+}
+</style>
